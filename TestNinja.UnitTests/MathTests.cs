efaa4fc64b55a1ac30460af08a4889a3caa7bb5f
Qassim Mohammed Alshakhoori <qalshakhoori@moic.gov.bh>
2019-12-04 08:35:57 +0300

    Lecture 27

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        private Math _math;
+
+        [SetUp]
+        public void Setup()
+        {
+            _math = new Math();
+        }
+
+        [Test]
+        //[Ignore("Test useful Ignore comment")]
+        public void Add_WhenCalled_ReturnSumOfArguments()
+        {
+            var result = _math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        [TestCase(1,2,2)]
+        [TestCase(2,1,2)]
+        [TestCase(1,1,1)]
+        public void Max_WhenCalled_ReturnTheGreaterArguement(int a, int b, int expectedResult)
+        {
+            var result = _math.Max(a, b);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
--- TestNinja/Fundamentals/Math.cs
+++ TestNinja/Fundamentals/Math.cs
@@ -5,7 +5,7 @@ namespace TestNinja.Fundamentals
     public class Math
     {
         public int Add(int a, int b)
-        { 
+        {
             return a + b;
         }
         
