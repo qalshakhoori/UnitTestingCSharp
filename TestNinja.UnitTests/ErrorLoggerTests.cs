4cce82241c19b1809f1801b68322e17c11472d57
Qassim Mohammed Alshakhoori <qalshakhoori@moic.gov.bh>
2019-12-05 09:14:51 +0300

    lecture 32

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLAstErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+    }
+}
