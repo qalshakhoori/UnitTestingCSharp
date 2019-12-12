dea928fd00e9c763967c5ccfb11983c92e09f136
Qassim Mohammed Alshakhoori <qalshakhoori@moic.gov.bh>
2019-12-12 09:45:52 +0300

    lecture 58

---
+++ TestNinja.UnitTests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using TestNinja.Mocking;
+using NUnit.Framework;
+using Moq;
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+        [Test]
+        public void PlaceOrder_WhenCalled_storeTheOrder()
+        {
+            var storage = new Mock<IStorage>();
+            var service = new OrderService(storage.Object);
+
+            var order = new Order();
+            service.PlaceOrder(order);
+
+            storage.Verify(s => s.Store(order));
+        }
+    }
+}
--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,18 +1,30 @@
 ﻿using NUnit.Framework;
 using TestNinja.Mocking;
-
+using Moq;
 namespace TestNinja.UnitTests.Mocking
 {
     [TestFixture]
     public class VideoServiceTests
     {
+        private VideoService _service;
+        private Mock<IFileReader> _fileReader;
+        [SetUp]
+        public void Setup()
+        {
+            _fileReader = new Mock<IFileReader>(); // Mocking Framework
+            _service = new VideoService(_fileReader.Object); // Pass Object via Mocking framework
+        }
+
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnErrorMessage()
         {
-            var service = new VideoService(new FakeFileReader()); // Dependencyc injection via Constructure
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+
+            //var service = new VideoService(new FakeFileReader()); // Dependencyc injection via Constructure
 
             //var result = service.ReadVideoTitle(new FakeFileReader()); // Dependencyc injection via Method Parameter
-            var result = service.ReadVideoTitle();
+
+            var result = _service.ReadVideoTitle();
 
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
